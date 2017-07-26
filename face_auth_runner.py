e02b30444998898b017ff9f00d49f1018ac2c601
Jack Pincus <jwpincus@gmail.com>
2017-07-26 16:24:54 -0600

    logic flow for relay switching

--- face_auth_runner.py
+++ face_auth_runner.py
@@ -9,17 +9,26 @@ GPIO.setmode(GPIO.BCM)
 
 buttonPin = 17
 GPIO.setup(buttonPin, GPIO.IN)
+relayPin = 18
+GPIO.setup(relayPin, GPIO.OUT)
+
+circuit = False
 auth_url = 'http://re-cognizer.herokuapp.com/api/v1/authenticate'
 base64_prefix = 'data:image/jpeg;base64,'
 
 while True:
-    if (GPIO.input(buttonPin)):
+    if (GPIO.input(buttonPin) and not circuit):
         filename = str(datetime.datetime.now())
         Camera(filename).snap()
         with open('./images/' + filename + '.jpg', "rb") as image:
             base64_image = base64.b64encode(image.read())
         response = requests.post(auth_url, data={ "app_key" : "f94a4394871ce63524cd", "image": base64_prefix + base64_image})
         print response.text
-        embed()
+        if (response.json()['authenticated'])
+            circuit = True
+            GPIO.output(relayPin, circuit)
+    else if (GPIO.input(buttonPin) and circuit):
+        circuit = False
+        GPIO.output(relayPin, circuit)
         # if authorized, turn on ciruit
             # on button push disable circuit
