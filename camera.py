4ca3dc9a82fea3f45a5cbed974230fe5aaab1f6f
Jack Pincus <jwpincus@gmail.com>
2017-07-26 15:07:06 -0600

    working on the thing

--- camera.py
+++ camera.py
@@ -3,12 +3,12 @@ from time import sleep
 
 
 class Camera(object):
-    camera = picamera.PiCamera()
 
     def __init__(self, filename):
         self.filename = filename
 
     def snap(self):
+        camera = picamera.PiCamera()
         camera.resolution = (800, 600)
         camera.start_preview()
         sleep(1)
