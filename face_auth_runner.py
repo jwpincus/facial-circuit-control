77aa1fd758a4002d1a787275a0e7515f7675fac3
Jack Pincus <jwpincus@gmail.com>
2017-07-26 15:40:34 -0600

    fix b64 string formatting

--- face_auth_runner.py
+++ face_auth_runner.py
@@ -9,15 +9,15 @@ GPIO.setmode(GPIO.BCM)
 buttonPin = 17
 GPIO.setup(buttonPin, GPIO.IN)
 auth_url = 'http://re-cognizer.herokuapp.com/api/v1/authenticate'
+base64_prefix = 'data:image/jpeg;base64,'
 
 while True:
     if (GPIO.input(buttonPin)):
         filename = str(datetime.datetime.now())
         Camera(filename).snap()
         with open('./images/' + filename + '.jpg', "rb") as image:
-            base64image = base64.b64encode(image.read())
-        response = requests.post(auth_url, data={ "app_key" : "f94a4394871ce63524cd", "image": base64image})
-        image = None
+            base64_image = base64.b64encode(image.read())
+        response = requests.post(auth_url, data={ "app_key" : "f94a4394871ce63524cd", "image": base64_prefix + base64_image})
         print response.text
 
 
