7b0930b0da6816d284134d84d74db69971e4f6c8
Jack Pincus <jwpincus@gmail.com>
2017-07-26 15:26:45 -0600

    add http call to API

--- face_auth_runner.py
+++ face_auth_runner.py
@@ -2,11 +2,13 @@ import RPi.GPIO as GPIO
 from camera import Camera
 import base64
 import datetime
+import requests
 
 GPIO.setmode(GPIO.BCM)
 
 buttonPin = 17
 GPIO.setup(buttonPin, GPIO.IN)
+auth_url = 'https://re-cognizer.herokuapp.com/api/v1/authenticate'
 
 while True:
     if (GPIO.input(buttonPin)):
@@ -14,8 +16,10 @@ while True:
         Camera(filename).snap()
         with open('./images/' + filename + '.jpg', "rb") as image:
             base64image = base64.b64encode(image.read())
-            print base64image
-        
+        response = requests.put(auth_url, data={ "app_key" : "f94a4394871ce63524cd", "image": base64image})
+        puts response
+
+
         # send to api
         # if authorized, turn on ciruit
             # on button push disable circuit
