106166dbe60bde464655b0bcc0b1118c59cf8aeb
Jack Pincus <jwpincus@gmail.com>
2017-07-29 12:01:18 -0600

    tone it down

--- distance_sensor.py
+++ distance_sensor.py
@@ -1,5 +1,5 @@
 from gpiozero import DistanceSensor
 
-ultrasonic = DistanceSensor(echo=24, trigger=17)
+ultrasonic = DistanceSensor(echo=24, trigger=23)
 while True:
         print ultrasonic.distance
