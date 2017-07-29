099a78a9db9c838c5fe2d1b1b28531549f7133bc
Jack Pincus <jwpincus@gmail.com>
2017-07-29 14:21:07 -0600

    try copy pasta

--- distance_sensor.py
+++ distance_sensor.py
@@ -1,35 +1,5 @@
-import RPi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BCM)
+from gpiozero import DistanceSensor
 
-TRIG = 23
-ECHO = 24
-
-print "Distance Measurement In Progress"
-
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
-
-GPIO.output(TRIG, False)
-print "Waiting For Sensor To Settle"
-time.sleep(2)
-
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
-
-while GPIO.input(ECHO)==0:
-    pulse_start = time.time()
-
-while GPIO.input(ECHO)==1:
-    pulse_end = time.time()
-
-pulse_duration = pulse_end - pulse_start
-
-distance = pulse_duration * 17150
-
-distance = round(distance, 2)
-
-print "Distance:",distance,"cm"
-
-GPIO.cleanup()
+ultrasonic = DistanceSensor(echo=24, trigger=23)
+while True:
+        print ultrasonic.distance
