ac4a084b8fa00ecbbc042bd2a879dd1e19b90416
Jack Pincus <jwpincus@gmail.com>
2017-07-29 15:13:09 -0600

    try copy pasta

--- library_distance.py
+++ library_distance.py
@@ -3,5 +3,5 @@ from time import sleep
 
 sensor = DistanceSensor(echo=21, trigger=20)
 while True:
-    print('Distance: ', sensor.distance() * 100)
+    print('Distance: ', sensor.distance() * 100.0)
     sleep(1)
