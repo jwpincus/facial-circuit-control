19a3e5d842991d773e7952026adb97e0c1efc62c
Jack Pincus <jwpincus@gmail.com>
2017-07-29 15:41:39 -0600

    try copy pasta

--- distance_sensor.py
+++ distance_sensor.py
@@ -1,35 +1,29 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
 
-TRIG = 20
-ECHO = 21
+class DistanceSensor:
+    GPIO.setmode(GPIO.BCM)
+    TRIG = 20
+    ECHO = 21
+    GPIO.setup(TRIG,GPIO.OUT)
+    GPIO.setup(ECHO,GPIO.IN)
+    GPIO.output(TRIG, False)
+    def measure(self):
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+        while GPIO.input(ECHO)==0:
+            pulse_start = time.time()
+        while GPIO.input(ECHO)==1:
+            pulse_end = time.time()
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17150
+        GPIO.cleanup()
+        distance = round(distance, 2)
+        return distance
 
-print "Distance Measurement In Progress"
+distance = DistanceSensor()
 
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
-
-GPIO.output(TRIG, False)
-print "Waiting For Sensor To Settle"
-time.sleep(2)
-
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
-
-while GPIO.input(ECHO)==0:
-  pulse_start = time.time()
-
-while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
-
-pulse_duration = pulse_end - pulse_start
-
-distance = pulse_duration * 17150
-
-distance = round(distance, 2)
-
-print "Distance:",distance,"cm"
-
-GPIO.cleanup()
+while True:
+    distance.measure
+    sleep(1)
