95f6c2b7632b5a31c5fecaa5b3881b6b257df905
Jack Pincus <jwpincus@gmail.com>
2017-07-29 15:25:33 -0600

    try copy pasta

--- library_distance.py
+++ library_distance.py
@@ -1,7 +1,7 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
-sensor = DistanceSensor(echo=21, trigger=20)
+sensor = DistanceSensor(21, 20)
 while True:
     print('Distance: ', sensor.distance * 100)
     sleep(1)
