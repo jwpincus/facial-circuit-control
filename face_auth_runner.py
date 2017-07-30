bdae5ff6e40cf6b4985aa9987f75f84a3f754b9f
Jack Pincus <jwpincus@gmail.com>
2017-07-30 11:47:11 -0600

    update api key

--- face_auth_runner.py
+++ face_auth_runner.py
@@ -32,7 +32,7 @@ while True:
         Camera(filename).snap()
         with open('/home/pi/facial-circuit-control/images/' + filename + '.jpg', "rb") as image: # I guess this is how to do it in python?
             base64_image = base64.b64encode(image.read()) # encode the image as base64 string
-        response = requests.post(auth_url, data={ "app_key" : "f94a4394871ce63524cd", "image": base64_prefix + base64_image})
+        response = requests.post(auth_url, data={ "app_key" : "c1f5af1651f412cd3e2d", "image": base64_prefix + base64_image})
         if (response.json()['authenticated']):
             circuit = True
             GPIO.output(relayPin, circuit)
